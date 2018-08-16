06133328dd92d5b4c544f79dc629debb63041ffd
niedengqiang <ndq371992308@163.com>
2018-08-16 16:35:06 +0800

    view

--- app/src/main/java/com/tomcat360/lyqb/activity/GenerateWalletActivity.java
+++ app/src/main/java/com/tomcat360/lyqb/activity/GenerateWalletActivity.java
@@ -1,6 +1,11 @@
 package com.tomcat360.lyqb.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,15 +14,23 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.tomcat360.lyqb.R;
 import com.tomcat360.lyqb.adapter.MnemonicWordAdapter;
 import com.tomcat360.lyqb.adapter.MnemonicWordHintAdapter;
+import com.tomcat360.lyqb.core.WalletHelper;
 import com.tomcat360.lyqb.utils.DialogUtil;
+import com.tomcat360.lyqb.utils.LyqbLogger;
+import com.tomcat360.lyqb.utils.ToastUtils;
 import com.tomcat360.lyqb.views.SpacesItemDecoration;
 import com.tomcat360.lyqb.views.TitleView;
 
+import org.web3j.crypto.Bip39Wallet;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -80,6 +93,8 @@ public class GenerateWalletActivity extends BaseActivity {
         setContentView(R.layout.activity_generate_wallet);
         ButterKnife.bind(this);
         super.onCreate(savedInstanceState);
+        initPermissions();
+        getFile();
 
     }
 
@@ -102,8 +117,8 @@ public class GenerateWalletActivity extends BaseActivity {
 
         recyclerMnemonicHint.setLayoutManager(layoutManagerHint);  //助记词提示列表
         List<String> listHint = new ArrayList<>();
-        for (int i = 1;i<13;i++){
-            listHint.add("mnemonic"+i);
+        for (int i = 1; i < 13; i++) {
+            listHint.add("mnemonic" + i);
         }
         mHintAdapter = new MnemonicWordHintAdapter(R.layout.adapter_mnemonic_word_hint, listHint);
         recyclerMnemonicHint.addItemDecoration(new SpacesItemDecoration(8));
@@ -112,18 +127,18 @@ public class GenerateWalletActivity extends BaseActivity {
 
         recyclerView.setLayoutManager(layoutManager);   //助记词选择列表
         List<String> list = new ArrayList<>();
-        for (int i = 1;i<13;i++){
-            list.add("mnemonic"+i);
+        for (int i = 1; i < 13; i++) {
+            list.add("mnemonic" + i);
         }
         mAdapter = new MnemonicWordAdapter(R.layout.adapter_mnemonic_word, list);
         recyclerView.addItemDecoration(new SpacesItemDecoration(8));
         recyclerView.setAdapter(mAdapter);
     }
 
-    private int i = 0;
+    private String nextStatus = "start";
     private int j = 0;
 
-    @OnClick({R.id.wallet_name, R.id.btn_next,R.id.btn_confirm, R.id.btn_skip})
+    @OnClick({R.id.wallet_name, R.id.btn_next, R.id.btn_confirm, R.id.btn_skip})
     public void onViewClicked(View view) {
 
         switch (view.getId()) {
@@ -131,18 +146,30 @@ public class GenerateWalletActivity extends BaseActivity {
 
                 break;
             case R.id.btn_next:
-                ++i;
-                if (i == 1) {
+
+                if (nextStatus.equals("start")) {
+                    nextStatus = "pasworrd";
                     walletName.setVisibility(View.GONE);
                     llPassword.setVisibility(View.VISIBLE);
                     password.setVisibility(View.VISIBLE);
-                } else if (i == 2) {
-                    llPassword.setVisibility(View.GONE);
-                    password.setVisibility(View.GONE);
-                    repeatPassword.setVisibility(View.VISIBLE);
-                } else {
-                    generatePartone.setVisibility(View.GONE);
-                    generateParttwo.setVisibility(View.VISIBLE);
+                } else if (nextStatus.equals("pasworrd")) {
+                    if (password.length() < 6) {
+                        ToastUtils.toast("请输入6位以上密码");
+                    } else {
+                        nextStatus = "match";
+                        llPassword.setVisibility(View.GONE);
+                        password.setVisibility(View.GONE);
+                        repeatPassword.setVisibility(View.VISIBLE);
+                    }
+                } else if (nextStatus.equals("match")) {
+                    if (repeatPassword.getText().toString().equals(password.getText().toString())) {
+//                            generatePartone.setVisibility(View.GONE);
+//                            generateParttwo.setVisibility(View.VISIBLE);
+                        ToastUtils.toast("成功");
+                        createWallet();
+                    } else {
+                        ToastUtils.toast("两次输入密码不一样");
+                    }
                 }
                 break;
             case R.id.btn_confirm:
@@ -151,7 +178,7 @@ public class GenerateWalletActivity extends BaseActivity {
                     rlMnemonic.setVisibility(View.GONE);
                     rlWord.setVisibility(View.GONE);
                     generatePartthree.setVisibility(View.VISIBLE);
-                }else {
+                } else {
                     DialogUtil.showWalletCreateResultDialog(this, new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
@@ -169,5 +196,80 @@ public class GenerateWalletActivity extends BaseActivity {
         }
     }
 
+    private void createWallet() {
+        try {
+            String pas = repeatPassword.getText().toString();
+            LyqbLogger.log(pas+"     ");
+            Bip39Wallet bip39Wallet = WalletHelper.create(pas, getFile());
+            LyqbLogger.log(pas+"     "+bip39Wallet.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private File getFile() {
+//在SD卡下建目录
+        String dir = Environment.getExternalStorageDirectory().getPath() + "/mnoe";
+        LyqbLogger.log(dir);
+        File mFile = new File(dir);
+        LyqbLogger.log(String.valueOf(mFile.exists()));
+        LyqbLogger.log(String.valueOf(mFile.isDirectory()));
+
+        if (!mFile.exists())
+            mFile.mkdirs();
+//        File mFile = new File("/mnt/sdcard/work/mywork");
+////判断文件夹是否存在，如果不存在就创建，否则不创建
+//        if (!mFile.exists()) {
+//            //通过file的mkdirs()方法创建目录中包含却不存在的文件夹
+//            mFile.mkdirs();
+////        }
+
+
+        LyqbLogger.log(String.valueOf(mFile.exists()));
+        LyqbLogger.log(String.valueOf(mFile.isDirectory()));
+        return mFile;
+    }
+
+
+    private void initPermissions() {
+        /**
+         //		 * 6.0系统 获取权限
+         //		 */
+        List<String> list = new ArrayList<>();
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            list.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+            list.add(Manifest.permission.CAMERA);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)) {
+            list.add(Manifest.permission.READ_CONTACTS);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)) {
+            list.add(Manifest.permission.CALL_PHONE);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.READ_LOGS)) {
+            list.add(Manifest.permission.READ_LOGS);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            list.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            list.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.SET_DEBUG_APP)) {
+            list.add(Manifest.permission.SET_DEBUG_APP);
+        }
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.SYSTEM_ALERT_WINDOW)) {
+            list.add(Manifest.permission.SYSTEM_ALERT_WINDOW);
+        }
+        if (list.size() > 0) {
+            String[] mPermissionList = list.toArray(new String[]{});
+            ActivityCompat.requestPermissions(this, mPermissionList, 100);
+        }
+
+
+    }
 
 }
