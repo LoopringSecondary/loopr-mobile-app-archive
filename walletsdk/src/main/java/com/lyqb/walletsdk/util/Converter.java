d3bfc685dfa5c57fc6761fe285c96c62d8741dd1
slice30k <slice30k@qq.com>
2018-08-30 16:02:05 +0800

    converter

--- walletsdk/src/main/java/com/lyqb/walletsdk/util/Converter.java
+++ walletsdk/src/main/java/com/lyqb/walletsdk/util/Converter.java
@@ -4,7 +4,8 @@ import org.web3j.utils.Convert;
 
 import java.math.BigInteger;
 
-public class ConvertUtils {
+public class Converter {
+
     public static BigInteger ethToWei(String value) {
         return Convert.toWei(value, Convert.Unit.ETHER).toBigInteger();
     }
