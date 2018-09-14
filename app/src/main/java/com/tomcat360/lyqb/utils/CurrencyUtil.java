7b3306385146b38eae9654d44d1b9fa3c4122a33
iwbrhwfy <kenshin_6@163.com>
2018-09-14 16:43:38 +0800

    modify currency utils

--- app/src/main/java/com/tomcat360/lyqb/utils/CurrencyUtil.java
+++ app/src/main/java/com/tomcat360/lyqb/utils/CurrencyUtil.java
@@ -1,15 +1,18 @@
-package com.tomcat360.lyqb.utils;
-
-import android.content.Context;
-
-import com.lyqb.walletsdk.model.Currency;
-
 /**
  * Created with IntelliJ IDEA.
  * User: kenshin wangchen@loopring.org
  * Time: 2018-09-11 下午6:01
  * Cooperation: loopring.org 路印协议基金会
  */
+package com.tomcat360.lyqb.utils;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import android.content.Context;
+
+import com.lyqb.walletsdk.model.Currency;
+
 public class CurrencyUtil {
 
     public static Currency getCurrency(Context context) {
@@ -23,5 +26,11 @@ public class CurrencyUtil {
         } else {
             return Currency.USD;
         }
+    }
+
+    public static String format(Context context, double value) {
+        Locale locale = getCurrency(context).getLocale();
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(locale);
+        return formatter.format(value);
     }
 }
--- walletsdk/src/main/java/com/lyqb/walletsdk/model/Currency.java
+++ walletsdk/src/main/java/com/lyqb/walletsdk/model/Currency.java
@@ -1,21 +1,26 @@
-package com.lyqb.walletsdk.model;
-
 /**
  * Created with IntelliJ IDEA.
  * User: kenshin wangchen@loopring.org
  * Time: 2018-09-11 下午6:32
  * Cooperation: loopring.org 路印协议基金会
  */
+package com.lyqb.walletsdk.model;
+
+import java.util.Locale;
+
 public enum Currency {
-    CNY("CNY", "￥"), USD("USD", "$");
+    CNY("CNY", "￥", Locale.CHINA), USD("USD", "$", Locale.US);
 
     private String text;
 
     private String symbol;
 
-    Currency(String text, String symbol) {
+    private Locale locale;
+
+    Currency(String text, String symbol, Locale locale) {
         this.text = text;
         this.symbol = symbol;
+        this.locale = locale;
     }
 
     public String getText() {
@@ -25,4 +30,7 @@ public enum Currency {
     public String getSymbol() {
         return symbol;
     }
+
+    public Locale getLocale() { return locale; }
+
 }
