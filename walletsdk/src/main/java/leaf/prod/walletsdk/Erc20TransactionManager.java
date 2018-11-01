af02580e099ee21cdf782b4ca9621e122fbfe447
iwbrhwfy <kenshin_6@163.com>
2018-11-01 14:14:29 +0800

    add get raw tx of erc20 token

--- walletsdk/src/main/java/leaf/prod/walletsdk/Erc20TransactionManager.java
+++ walletsdk/src/main/java/leaf/prod/walletsdk/Erc20TransactionManager.java
@@ -1,18 +1,33 @@
 package leaf.prod.walletsdk;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
 
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Int;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.Contract;
 import org.web3j.tx.TransactionManager;
-import leaf.prod.walletsdk.api.Erc20Contract;
 
 import leaf.prod.walletsdk.api.Erc20Contract;
 
 public class Erc20TransactionManager {
+
     private static final BigInteger gasPrice = Contract.GAS_PRICE; //22000000000
-//    private static final BigInteger gasLimit = Contract.GAS_LIMIT; //4300000
+
+    //    private static final BigInteger gasLimit = Contract.GAS_LIMIT; //4300000
     private static final BigInteger gasLimit = BigInteger.valueOf(100000); //weth转账最低limit
+
+    private Web3j web3j = SDK.getWeb3j();
 
     private Erc20Contract erc20Contract;
 
@@ -48,5 +63,16 @@ public class Erc20TransactionManager {
         erc20Contract.setGasPrice(gasPrice);
         TransactionReceipt transactionReceipt = erc20Contract.transfer(to, value).send();
         return transactionReceipt.getTransactionHash();
+    }
+
+    public RawTransaction getRawTransaction(Credentials credentials, String to, String contractAddress, BigInteger gasPrice, BigInteger gasLimit, BigInteger value) throws IOException {
+        Function function = new Function("transfer",
+                Arrays.asList(new Address(to), new Int(value)),
+                Collections.emptyList());
+        String data = FunctionEncoder.encode(function);
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
+                credentials.getAddress(), DefaultBlockParameterName.PENDING).send();
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+        return RawTransaction.createTransaction(nonce, gasPrice, gasLimit, contractAddress, data);
     }
 }
