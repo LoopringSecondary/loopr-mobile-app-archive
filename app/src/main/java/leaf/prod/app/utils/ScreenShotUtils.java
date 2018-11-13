dbe86fa1524e0b8c9dea22248eed26194096fc62
laiyanyan <liulanggou917@126.com>
2018-11-13 17:15:59 +0800

    生成钱包界面助记词禁止截屏

---
+++ app/src/main/java/leaf/prod/app/utils/ScreenShotUtils.java
@@ -0,0 +1,11 @@
+package leaf.prod.app.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: laiyanyan
+ * Time: 2018-11-13 5:08 PM
+ * Cooperation: loopring.org 路印协议基金会
+ */
+public class ScreenShotUtils {
+
+}
