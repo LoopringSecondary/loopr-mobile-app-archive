1882625191b26a8cdf80e55c0f12d4e7a4febccb
laiyanyan <liulanggou917@126.com>
2018-11-27 10:28:33 +0800

    第三方登录地址切换

--- walletsdk/src/main/java/leaf/prod/walletsdk/SDK.java
+++ walletsdk/src/main/java/leaf/prod/walletsdk/SDK.java
@@ -27,8 +27,7 @@ public class SDK {
 
     private static String ETH_BASE = "https://relay1.loopring.io/eth";
 
-    //    private static String APP_SERVICE_BASE = "https://www.loopring.mobi";
-    private static String APP_SERVICE_BASE = "http://10.137.110.6:5000";
+    private static String APP_SERVICE_BASE = "https://www.loopring.mobi";
 
     private static Web3j web3j = null;
 
