679823db0a6c1d80522431c5753ffd2e596b3e8d
laiyanyan <liulanggou917@126.com>
2018-12-04 16:03:48 +0800

    p2p结果页

---
+++ app/src/main/java/leaf/prod/app/activity/P2PSuccessActivity.java
@@ -0,0 +1,58 @@
+package leaf.prod.app.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import leaf.prod.app.R;
+import leaf.prod.app.views.TitleView;
+
+public class P2PSuccessActivity extends BaseActivity {
+
+    @BindView(R.id.title)
+    TitleView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_p2p_success);
+        ButterKnife.bind(this);
+        super.onCreate(savedInstanceState);
+        mSwipeBackLayout.setEnableGesture(false);
+    }
+
+    @Override
+    protected void initPresenter() {
+    }
+
+    @Override
+    public void initTitle() {
+        title.setBTitleNoBar(getResources().getString(R.string.trade_result));
+    }
+
+    @Override
+    public void initView() {
+    }
+
+    @Override
+    public void initData() {
+    }
+
+    @OnClick({R.id.btn_return, R.id.btn_view_orders})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_return:
+                getOperation().forward(MainActivity.class);
+                break;
+            case R.id.btn_view_orders:
+                // todo 查看订单
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        getOperation().forward(MainActivity.class);
+    }
+}
