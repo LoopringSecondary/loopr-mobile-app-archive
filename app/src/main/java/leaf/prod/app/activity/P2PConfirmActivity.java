b83e3e282e1e41e1fea93f99991dec024152a55d
iwbrhwfy <kenshin_6@163.com>
2018-12-05 19:25:17 +0800

    modify p2p

--- app/src/main/java/leaf/prod/app/activity/P2PConfirmActivity.java
+++ app/src/main/java/leaf/prod/app/activity/P2PConfirmActivity.java
@@ -7,15 +7,62 @@
 package leaf.prod.app.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import leaf.prod.app.R;
+import leaf.prod.app.presenter.P2PConfirmPresenter;
 
 public class P2PConfirmActivity extends BaseActivity {
 
+    @BindView(R.id.iv_token_s)
+    public ImageView tokenSImage;
+
+    @BindView(R.id.iv_token_b)
+    public ImageView tokenBImage;
+
+    @BindView(R.id.tv_sell_token)
+    public TextView tokenSText;
+
+    @BindView(R.id.tv_buy_token)
+    public TextView tokenBText;
+
+    @BindView(R.id.tv_sell_price)
+    public TextView sellPriceText;
+
+    @BindView(R.id.tv_buy_price)
+    public TextView buyPriceText;
+
+    @BindView(R.id.tv_sell_amount)
+    public TextView sellAmountText;
+
+    @BindView(R.id.tv_buy_amount)
+    public TextView buyAmountText;
+
+    @BindView(R.id.tv_price)
+    public TextView priceText;
+
+    @BindView(R.id.tv_trading_fee)
+    public TextView tradingFeeText;
+
+    @BindView(R.id.tv_margin_split)
+    public TextView marginSplitText;
+
+    @BindView(R.id.tv_live_time)
+    public TextView liveTimeText;
+
+    private P2PConfirmPresenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_p2p_success);
+        setContentView(R.layout.dialog_p2p_trade_detail);
         ButterKnife.bind(this);
         super.onCreate(savedInstanceState);
     }
@@ -25,6 +72,7 @@ public class P2PConfirmActivity extends BaseActivity {
      */
     @Override
     protected void initPresenter() {
+        presenter = new P2PConfirmPresenter(this, this);
     }
 
     /**
@@ -47,5 +95,22 @@ public class P2PConfirmActivity extends BaseActivity {
      */
     @Override
     public void initData() {
+        String p2pOrder = getIntent().getStringExtra("p2p_order");
+        if (p2pOrder != null && !p2pOrder.isEmpty()) {
+            JsonObject object = new JsonParser().parse(p2pOrder).getAsJsonObject();
+            presenter.p2pContent = object.get("value").getAsJsonObject();
+            presenter.handleResult();
+        }
+    }
+
+    @OnClick({R.id.btn_order, R.id.btn_cancel})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_order:
+                presenter.processTaker();
+                break;
+            case R.id.cancel:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/leaf/prod/app/presenter/P2PConfirmPresenter.java
@@ -0,0 +1,70 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kenshin wangchen@loopring.org
+ * Time: 2018-12-05 5:39 PM
+ * Cooperation: loopring.org 路印协议基金会
+ */
+package leaf.prod.app.presenter;
+
+import android.content.Context;
+
+import com.google.gson.JsonObject;
+
+import leaf.prod.app.activity.P2PConfirmActivity;
+import leaf.prod.app.utils.PasswordDialogUtil;
+import leaf.prod.walletsdk.manager.P2POrderDataManager;
+import leaf.prod.walletsdk.model.OriginOrder;
+import leaf.prod.walletsdk.util.WalletUtil;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class P2PConfirmPresenter extends BasePresenter<P2PConfirmActivity> {
+
+    public JsonObject p2pContent;
+
+    private P2POrderDataManager p2pManager;
+
+    public P2PConfirmPresenter(P2PConfirmActivity view, Context context) {
+        super(view, context);
+        p2pManager = P2POrderDataManager.getInstance(context);
+    }
+
+    public void handleResult() {
+        p2pManager.handleResult(p2pContent);
+        OriginOrder taker = p2pManager.getOrders()[1];
+        /*
+        view.tokenBText.setText(taker.getTokenB());  TODO: yanyan
+        */
+    }
+
+    public void processTaker() {
+        if (WalletUtil.needPassword(context)) {
+            PasswordDialogUtil.showPasswordDialog(context, v -> processTaker(PasswordDialogUtil.getInputPassword()));
+        } else {
+            processTaker("");
+        }
+    }
+
+    private void processTaker(String password) {
+        try {
+            p2pManager.verify(password);
+        } catch (Exception e) {
+            // TODO: for yanyan: MUST handle exception of incorrect password
+            e.printStackTrace();
+        }
+        if (!p2pManager.isBalanceEnough()) {
+            // TODO: for yanyan: balance not enough
+            // p2pOrderManager.balanceInfo e.g. {"MINUS_ETH": 0.3974, "MINUS_LRC": 10.3974}
+        }
+        p2pManager.handleInfo()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> {
+                    if (response.getError() == null) {
+                        // TODO: for yanyan submit order success
+                    } else {
+                        // TODO: for yanyan submit order failed
+                    }
+                });
+    }
+}
