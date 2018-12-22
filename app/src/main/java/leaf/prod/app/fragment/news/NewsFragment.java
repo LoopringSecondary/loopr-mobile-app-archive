9e42014258779647c76e66a2e80b5340c9f99e22
iwbrhwfy <kenshin_6@163.com>
2018-12-22 15:10:31 +0800

    new news feature

---
+++ app/src/main/java/leaf/prod/app/fragment/news/NewsFragment.java
@@ -0,0 +1,52 @@
+package leaf.prod.app.fragment.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import leaf.prod.app.R;
+import leaf.prod.app.fragment.BaseFragment;
+
+/**
+ *
+ */
+public class NewsFragment extends BaseFragment {
+
+    Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // 布局导入
+        layout = inflater.inflate(R.layout.fragment_news, container, false);
+        unbinder = ButterKnife.bind(this, layout);
+        return layout;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    protected void initPresenter() {
+    }
+
+    @Override
+    protected void initView() {
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
