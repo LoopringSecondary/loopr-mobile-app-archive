6987adf7946a04b3da822a50acdfae13f9608ed9
xiaowheat <eth_dev@126.com>
2018-12-21 23:20:32 -0800

    Update the UI in selecting contact.

--- loopr-ios/Wallet/SendAsset/ContactInSendAssetViewControllerTableViewCell.swift
+++ loopr-ios/Wallet/SendAsset/ContactInSendAssetViewControllerTableViewCell.swift
@@ -12,10 +12,29 @@ class ContactInSendAssetViewControllerTableViewCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var seperateLine: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        selectionStyle = .none
+        
+        seperateLine.theme_backgroundColor = ColorPicker.cardBackgroundColor
+        nameLabel.lineBreakMode = .byTruncatingMiddle
+        nameLabel.font = FontConfigManager.shared.getMediumFont(size: 14)
+        // nameLabel.theme_textColor = GlobalPicker.textColor
+        
+        addressLabel.lineBreakMode = .byTruncatingMiddle
+        addressLabel.font = FontConfigManager.shared.getMediumFont(size: 14)
+        // addressLabel.theme_textColor = GlobalPicker.textLightColor
+    }
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        if highlighted {
+            backgroundColor = UIColor.dark5
+        } else {
+            backgroundColor = .white
+        }
     }
     
     func update(contact: Contact) {
