c827bd3f4dc702afe060e188fa4b7a46523af9e9
xiaowheat <eth_dev@126.com>
2018-12-24 02:04:27 -0800

    Show claim date in AirdropView

--- loopr-ios/Wallet/Airdrop/AirdropViewController.swift
+++ loopr-ios/Wallet/Airdrop/AirdropViewController.swift
@@ -15,10 +15,17 @@ class AirdropViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentViewHeightLayoutConstraint: NSLayoutConstraint!
+    let heightForClaim: CGFloat = 270
+    let heightForComingBackTomorrow: CGFloat = 370
+    
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var addressTipLabel: UILabel!
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var amountTipLabel: UILabel!
+    @IBOutlet weak var nextClaimDateTextField: UITextField!
+    @IBOutlet weak var nextClaimDateTipLabel: UILabel!
+
     @IBOutlet weak var claimButton: GradientButton!
     @IBOutlet weak var forwardButton: GradientButton!
     var isFirtTimeAppear: Bool = true
@@ -44,6 +51,7 @@ class AirdropViewController: UIViewController, UIScrollViewDelegate {
         addressTextField.theme_tintColor = GlobalPicker.contrastTextColor
         addressTextField.contentMode = UIViewContentMode.bottom
         addressTextField.setLeftPaddingPoints(8)
+        addressTextField.isUserInteractionEnabled = false
         
         addressTipLabel.font = FontConfigManager.shared.getCharactorFont(size: 12)
         addressTipLabel.theme_textColor = GlobalPicker.textLightColor
@@ -53,16 +61,36 @@ class AirdropViewController: UIViewController, UIScrollViewDelegate {
         amountTextField.theme_tintColor = GlobalPicker.contrastTextColor
         amountTextField.contentMode = UIViewContentMode.bottom
         amountTextField.setLeftPaddingPoints(8)
+        amountTextField.isUserInteractionEnabled = false
         
         amountTipLabel.font = FontConfigManager.shared.getCharactorFont(size: 12)
         amountTipLabel.theme_textColor = GlobalPicker.textLightColor
         amountTipLabel.text = LocalizedString("Airdrop amount tip", comment: "")
 
+        nextClaimDateTextField.font = FontConfigManager.shared.getDigitalFont(size: 14)
+        nextClaimDateTextField.theme_tintColor = GlobalPicker.contrastTextColor
+        nextClaimDateTextField.contentMode = UIViewContentMode.bottom
+        nextClaimDateTextField.setLeftPaddingPoints(8)
+        nextClaimDateTextField.isUserInteractionEnabled = false
+        
+        nextClaimDateTipLabel.font = FontConfigManager.shared.getCharactorFont(size: 12)
+        nextClaimDateTipLabel.theme_textColor = GlobalPicker.textLightColor
+        nextClaimDateTipLabel.text = LocalizedString("Airdrop next claim date", comment: "")
+
+        nextClaimDateTextField.isHidden = true
+        nextClaimDateTipLabel.isHidden = true
+        
         claimButton.title = LocalizedString("Airdrop button", comment: "")
+        claimButton.isHidden = false
         forwardButton.title = LocalizedString("Airdrop forward", comment: "")
 
+        contentViewHeightLayoutConstraint.constant = heightForClaim
+        
         scrollView.delegate = self
         scrollView.delaysContentTouches = false
+        
+        // No need to scroll the scrollView at this moment.
+        scrollView.isScrollEnabled = false
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -101,8 +129,9 @@ class AirdropViewController: UIViewController, UIScrollViewDelegate {
                     SVProgressHUD.dismiss()
                     let notificationTitle = LocalizedString("Airdrop no bind", comment: "")
                     let banner = NotificationBanner.generate(title: notificationTitle, style: .danger)
-                    banner.duration = 5.0
+                    banner.duration = 3.0
                     banner.show()
+                    self.claimButton.isHidden = false
                 }
             }
         }
@@ -144,14 +173,31 @@ class AirdropViewController: UIViewController, UIScrollViewDelegate {
         var result = false
         let date = UserDefaults.standard.object(forKey: "\(UserDefaultsKeys.airdropDate.rawValue)\(self.bindAddress!)")
         if let date = date as? Date {
+            // The date has been added a 24 hours.
             let target = Calendar.current.date(byAdding: .hour, value: 24, to: date)
             if Date() < target! {
                 self.claimButton.isHidden = true
                 self.forwardButton.isHidden = false
+                self.bindAmount = "0"
                 result = false
+                contentViewHeightLayoutConstraint.constant = heightForComingBackTomorrow
+                nextClaimDateTextField.isHidden = false
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                let stringGivenDate = dateFormatter.string(from: target!)
+                nextClaimDateTextField.text = stringGivenDate
+                nextClaimDateTipLabel.isHidden = false
             } else {
                 self.claimButton.isHidden = false
                 self.forwardButton.isHidden = true
+                contentViewHeightLayoutConstraint.constant = heightForComingBackTomorrow
+                nextClaimDateTextField.isHidden = false
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                let stringGivenDate = dateFormatter.string(from: date)
+                nextClaimDateTextField.text = stringGivenDate
+                nextClaimDateTipLabel.text = LocalizedString("Airdrop last claim date", comment: "")
+                nextClaimDateTipLabel.isHidden = false
                 result = true
             }
         } else {
