20ee09447b8ba9e0fab80ffa6726ac50c40f4de0
xiaowheat <eth_dev@126.com>
2018-12-31 13:55:32 -0800

    Add ModelPushPopAnimator

--- loopr-ios/News/NewsDetail/NewsDetailViewController.swift
+++ loopr-ios/News/NewsDetail/NewsDetailViewController.swift
@@ -22,6 +22,25 @@ class NewsDetailViewController: UIViewController, WKNavigationDelegate {
     fileprivate let userCardPresentAnimationController = NewsDetailPresentAnimationController()
     fileprivate let userCardDismissAnimationController = NewsDetailDismissAnimationController()
     
+    var edgeView: UIView? {
+        get {
+            if (_edgeView == nil && isViewLoaded) {
+                _edgeView = UIView()
+                _edgeView?.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview(_edgeView!)
+                _edgeView?.backgroundColor = UIColor(white: 1.0, alpha: 0.005)
+                let bindings = ["edgeView": _edgeView!]
+                let options = NSLayoutFormatOptions(rawValue: 0)
+                let hConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-0-[edgeView(5)]", options: options, metrics: nil, views: bindings)
+                let vConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[edgeView]-0-|", options: options, metrics: nil, views: bindings)
+                view?.addConstraints(hConstraints)
+                view?.addConstraints(vConstraints)
+            }
+            return _edgeView
+        }
+    }
+    private var _edgeView: UIView?
+    
     override open func viewDidLoad() {
         super.viewDidLoad()
 
