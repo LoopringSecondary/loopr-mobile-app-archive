7caf9669791471f7ff4c03ca3b07ce0221b91f0c
xiaowheat <eth_dev@126.com>
2019-03-17 00:10:27 -0700

    Add flutter_candlesticks

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'package:flutter_candlesticks/flutter_candlesticks.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +48,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
+  List sampleData = [
+    {"open":50.0, "high":100.0, "low":40.0, "close":80, "volumeto":5000.0},
+    {"open":80.0, "high":90.0, "low":55.0, "close":65, "volumeto":4000.0},
+    {"open":65.0, "high":120.0, "low":60.0, "close":90, "volumeto":7000.0},
+    {"open":90.0, "high":95.0, "low":85.0, "close":80, "volumeto":2000.0},
+    {"open":80.0, "high":85.0, "low":40.0, "close":50, "volumeto":3000.0},
+  ];
 
   void _incrementCounter() {
     setState(() {
@@ -105,6 +114,15 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
+            ),
+            new Container(
+              height: 200.0,
+              width: 200,
+              child: new OHLCVGraph(
+                  data: sampleData,
+                  enableGridLines: false,
+                  volumeProp: 0.2
+              ),
             ),
           ],
         ),
